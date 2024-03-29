75d9afab881d29d988e54d8b92607ca89b5c744e
Administrator <Administrator@Inspection-Team.proline.com.pk>
2024-03-29 23:45:32 +0000

    Place holder to display the lives and health of the player. I made changes to the health Panel by creating a new arrayList lifeLabels and a JLabel lifeLabel. I created a for loop to add seven lifelines to the JLabel and then added it to the arrayList.

--- src/hangman/HealthPanel.java
+++ src/hangman/HealthPanel.java
@@ -2,39 +2,47 @@ package hangman;
 
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.util.ArrayList;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class HealthPanel extends JPanel {
-	private ArrayList<JLabel>lifeLabels;
+private ArrayList<JLabel>lifeLabels;
 	
 	public HealthPanel()
 	{
-		setLayout(new FlowLayout());
+		setLayout(new GridLayout(1,7,4,1));
 		lifeLabels = new ArrayList<>();
-		for(int i = 0; i < 7; i++)
+		for(int i = 1; i <= 7; i++)
 		{
-			JLabel lifeLabel = new JLabel("❤");
-			lifeLabel.setForeground(Color.GREEN);
+			JLabel lifeLabel = new JLabel(String.valueOf(i));
+			lifeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+			lifeLabel.setBackground(Color.GREEN);
+			lifeLabel.setOpaque(true);
 			add(lifeLabel);
 			lifeLabels.add(lifeLabel);
+		
 		}
 	}
-		public void removeLife()
+		public boolean removeLife()
 		{
 			for (JLabel lifeLabel : lifeLabels)
 			{
 			if (lifeLabel.getForeground() == Color.GREEN)
 			{
 				lifeLabel.setForeground(Color.GREEN);
+				return true;
 				
 			}
 			
 			lifeLabel.setForeground(Color.RED);
-			break;
+			
+			
 			}
+			return false;
 		}
 	
 	
@@ -51,4 +59,5 @@ public class HealthPanel extends JPanel {
 		}
 	}
 	
+
 
