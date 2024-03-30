76a01b2ce2bfca98e7852911bde99f1dac3e3450
Administrator <Administrator@Inspection-Team.proline.com.pk>
2024-03-30 00:46:12 +0000

    Ive instantiated the mainwindow

--- src/hangman/Run.java
+++ src/hangman/Run.java
@@ -1,9 +1,11 @@
 package hangman;
 
-public class Run {
+import javax.swing.JFrame;
 
+public class Run  {
+	
 	public static void main(String[] args) {
-		
+	MainWindow mainWindow =  new MainWindow();
 	}
 
 }
